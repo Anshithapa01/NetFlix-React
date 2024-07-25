a90355eb825216a41bb4fc71e0ef5ddb5bbfd7a5
Anshitha <anshithapa01@gmail.com>
2024-07-25 10:46:41 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {action,originals} from './urls'
+import Banner from './Components/Banner/Banner';
+import Navbar from "./Components/Nav/Navbar";
+import RowPost from './Components/Row/RowPost';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <Banner/>
+      <RowPost url={originals} title='Netflix Originals'/>
+      <RowPost url={action} title='Action' isSmall/>
     </div>
   );
 }
